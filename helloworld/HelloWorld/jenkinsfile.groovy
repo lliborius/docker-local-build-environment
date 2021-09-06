fd151c2b05e39c9bec4a53a3d6a3a5ed3f7a15d1
rlrajiv <rlrajiv@gmail.com>
2021-09-07 02:12:01 +0530

    Update jenkinsfile.groovy

--- helloworld/HelloWorld/jenkinsfile.groovy
+++ helloworld/HelloWorld/jenkinsfile.groovy
@@ -1,1 +1,29 @@
-
+pipeline {
+	agent any
+	tools {
+		maven 'maven LATEST'
+		ant 'ant latest'
+		jdk 'AIX64 JDK8'
+	} 
+		environment {
+        SCM_CREDENTIALS = credentials('Jenkins_GIT')
+    }
+   stages {
+      stage('Preparation') { // for display purposes
+         steps {
+            // Get source from GIT repository.
+            git  branch: 'master', credentialsId: 'Jenkins_GIT	', poll: true, changelog: true, url: 'https://github.com/lliborius/docker-local-build-environment'
+         }
+      }
+      stage('Build') {
+         steps {
+            // get the application version from pom
+            pushPomVersionToEnv('helloworld/HelloWorld/pom.xml')
+            // Run the maven build
+            ansiColor('xterm') {
+            	sh "mvn -f request-handler/pom.xml -B versions:set -DnewVersion=${APP_SNAPSHOT_VERSION} -DenableJenkinsProfile=true"
+            	sh "mvn clean install package -f request-handler/pom.xml -e -U -fae -DenableJenkinsProfile=true -DfailIfNoTests=false -DskipTests=false -Dapplication.buildNumber=${BUILD_NUMBER}"
+            }
+			
+		 }		
+}
