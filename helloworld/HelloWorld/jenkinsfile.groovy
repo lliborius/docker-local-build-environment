b7605dcacdaf7b7af1924d2f82a5d1b3f0dd8303
rlrajiv <rlrajiv@gmail.com>
2021-09-08 01:59:46 +0530

    .

--- helloworld/HelloWorld/jenkinsfile.groovy
+++ helloworld/HelloWorld/jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline {
             // get the application version from pom
             //pushPomVersionToEnv('helloworld/HelloWorld/pom.xml')
             // Run the maven build
-            ansiColor('xterm') {
+            // ansiColor('xterm') {
             	sh "mvn -f helloworld/HelloWorld/pom.xml -B versions:set -DnewVersion=${APP_SNAPSHOT_VERSION} -DenableJenkinsProfile=true"
             	sh "mvn clean install package -f helloworld/HelloWorld/pom.xml -e -U -fae -DenableJenkinsProfile=true -DfailIfNoTests=false -DskipTests=false -Dapplication.buildNumber=${BUILD_NUMBER}"
             }
